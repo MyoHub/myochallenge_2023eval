9c217f64ed94295b646003a1d20dbad38f28d746
Pierre Schumacher <pierre.schumacher@tuebingen.mpg.de>
2023-07-17 23:54:16 +0200

    made deprl baselines compatible

--- agent/agent_mani_deprl.py
+++ agent/agent_mani_deprl.py
@@ -1,8 +1,9 @@
 import os
 import pickle
 import time
-from utils import RemoteConnection
+from utils import RemoteConnection, DummyEnv
 import numpy as np
+
 
 def get_custom_observation(rc):
     """
@@ -13,17 +14,20 @@ def get_custom_observation(rc):
     """
     # example of obs_keys for deprl baseline
     obs_keys = [
-      'internal_qpos',
-      'internal_qvel',
-      'muscle_length',
-      'muscle_velocity',
-      'muscle_force',
+            'hand_qpos', 
+            'hand_qvel', 
+            'obj_pos', 
+            'goal_pos', 
+            'pos_err', 
+            'obj_rot', 
+            'goal_rot', 
+            'rot_err'
     ]
     obs_keys.append('act')
 
     obs_dict = rc.get_obsdict()
     # add new features here that can be computed from obs_dict
-    obs_dict['qpos_without_xy'] = np.array(obs_dict['internal_qpos'][2:35].copy())
+    # obs_dict['qpos_without_xy'] = np.array(obs_dict['internal_qpos'][2:35].copy())
 
     return rc.obsdict2obsvec(obs_dict, obs_keys)
 
@@ -33,9 +37,9 @@ time.sleep(60)
 LOCAL_EVALUATION = os.environ.get("LOCAL_EVALUATION")
 
 if LOCAL_EVALUATION:
-    rc = RemoteConnection("environment:8086")
+    rc = RemoteConnection("environment:8085")
 else:
-    rc = RemoteConnection("localhost:8086")
+    rc = RemoteConnection("localhost:8085")
 
 
 
@@ -49,8 +53,8 @@ rc.set_observation_space(shape)
 ## HERE an example from a previously trained policy with deprl is shown (see https://github.com/facebookresearch/myosuite/blob/main/docs/source/tutorials/4a_deprl.ipynb)
 ## additional dependences such as gym and deprl might be needed
 import deprl
-policy = deprl.load_baseline(rc)
-print('CHASE-TAG agent: policy loaded')
+policy = deprl.load_baseline(DummyEnv(env_name='myoChallengeRelocateP1-v0', stub=rc))
+print('Relocate agent: policy loaded')
 ################################################
 
 
@@ -64,7 +68,7 @@ while not flag_completed:
     while not flag_trial :
 
         if counter == 0:
-            print('CHASE-TAG: Trial #'+str(repetition)+'Start Resetting the environment and get 1st obs')
+            print('Relocate: Trial #'+str(repetition)+'Start Resetting the environment and get 1st obs')
             obs = rc.reset()
 
         ################################################
@@ -80,6 +84,6 @@ while not flag_completed:
         flag_trial = base["feedback"][2]
         flag_completed = base["eval_completed"]
 
-        print(f"CHASE-TAG: Agent Feedback iter {counter} -- trial solved: {flag_trial} -- task solved: {flag_completed}")
+        print(f"Relocate: Agent Feedback iter {counter} -- trial solved: {flag_trial} -- task solved: {flag_completed}")
         print("*" * 100)
         counter +=1
