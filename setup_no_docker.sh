5ae27e5cebb41c2692da7cec24456e8568faff5f
Pierre Schumacher <pschumacher@tuebingen.mpg.de>
2023-07-26 19:15:47 +0200

    updated setup_no_docker for new structure

--- setup_no_docker.sh
+++ setup_no_docker.sh
@@ -28,19 +28,19 @@ fi
 if [ "$PassEvalAI" = "true" ] && [ "$PassDocker" = "true" ]; then
     # Install required dependences
     pip install --upgrade pip
-    pip install torch
-    pip install -r requirements/agent.txt  -f https://download.pytorch.org/whl/torch_stable.html
+    pip install torch --index-url https://download.pytorch.org/whl/cpu
+    pip install -r requirements/agent_random.txt
     pip install grpcio grpcio-tools
-    pip install git+https://github.com/facebookresearch/myosuite.git@dev
+    pip install myosuite==1.7.0
 
     export PYTHONPATH="./utils/:$PYTHONPATH"
     export PYTHONPATH="./agent/:$PYTHONPATH"
     export PYTHONPATH="./environment/:$PYTHONPATH"
 
 
-    chmod u+r+x ./test/test_mani_agent.sh
-    chmod u+r+x ./test/test_loco_agent.sh
+    chmod u+r+x ./test/test_mani_agent_random.sh
+    chmod u+r+x ./test/test_loco_agent_random.sh
 
-    # ./test/test_mani_agent.sh
-    ./test/test_loco_agent.sh
+    ./test/test_mani_agent_random.sh
+    ./test/test_loco_agent_random.sh
 fi
